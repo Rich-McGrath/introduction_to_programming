45b2fb925dcf6d57744f1aaa3708a5f3d14f3f67
Rich-McGrath <richmcgrath@gmail.com>
2016-08-10 14:08:47 -0400

    exercise 1 completed

---
+++ arrays/exercise_1.rb
@@ -0,0 +1,16 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+puts "#{arr.include?(number)} the array does contain #{number}."
+
+# Below are the book examples
+
+arr.each do |num|
+  if num == number
+    puts "#{number} is in the array"
+  end
+end
+
+# ...or...
+if arr.include?(number)
+  puts "#{number} is indeed in the array"
+end
