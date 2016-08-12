1c373f229a4b2cb0505e8bca475e9be425c9abfe
Rich-McGrath <richmcgrath@gmail.com>
2016-08-12 13:26:55 -0400

    refactored exercise 1

--- more_stuff/exercise_1.rb
+++ more_stuff/exercise_1.rb
@@ -1,8 +1,11 @@
+def check_word(word)
+  if /lab/.match(word.to_s) != nil
+    puts "The sequence of characters 'lab' exists!"
+  else
+    puts "#{word} does not contain the sequence of characters 'lab'"
+  end
+end
+
 puts 'Please enter in a word:'
 word = gets.chomp
-
-if /lab/.match(word.to_s) != nil
-  puts "The sequence of characters 'lab' exists!"
-else
-  puts "#{word} does not contain the sequence of characters 'lab'"
-end
+check_word(word)
